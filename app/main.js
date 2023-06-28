31e46975b85bbea00411c916ac9b459eab42a230
Barry Carlyon <barry@barrycarlyon.co.uk>
2023-06-28 16:05:22 +0100

    Bumpb version, make electron sec. happy :-P

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, shell, dialog } = require('electron');
+const { app, BrowserWindow, ipcMain, shell, dialog, session  } = require('electron');
 const os = require('os');
 
 const fs = require('fs');
@@ -34,6 +34,15 @@ if (os.platform() == 'win32') {
 }
 
 app.on('ready', () => {
+    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+        callback({
+            responseHeaders: {
+                ...details.responseHeaders,
+                'Content-Security-Policy': ['script-src \'self\'']
+            }
+        })
+    });
+
     // settings migration
     let options = {
         width: 600,
